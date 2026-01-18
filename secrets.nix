19311f27e16a14df735090965f6badd225953cac
Riadh <22998716+KiKoS0@users.noreply.github.com>
2026-01-18 05:08:19 +0100

    nixos

--- secrets.nix
+++ secrets.nix
@@ -1,9 +1,11 @@
 let
   riadh =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJWD9cggIIfWOBaavJbmbWxrs9Ynol40ZWc02FWsXPia";
-  users = [ riadh ];
+  nixos =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJGIZS8YjCHiSlrRPa6Kbb8elSIEt7yfYAZDSer90k2d";
+  users = [ riadh nixos ];
   systems = [ ];
 in {
-  "openai_api_key.age".publicKeys = [ riadh ];
-  "claude_api_key.age".publicKeys = [ riadh ];
+  "openai_api_key.age".publicKeys = users;
+  "claude_api_key.age".publicKeys = users;
 }
