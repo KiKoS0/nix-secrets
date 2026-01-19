1e441bfa18f74611c7032c00805b342cce888409
Riadh <22998716+KiKoS0@users.noreply.github.com>
2026-01-19 01:14:08 +0100

    update nixos key

--- secrets.nix
+++ secrets.nix
@@ -2,7 +2,7 @@ let
   riadh =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJWD9cggIIfWOBaavJbmbWxrs9Ynol40ZWc02FWsXPia";
   nixos =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJGIZS8YjCHiSlrRPa6Kbb8elSIEt7yfYAZDSer90k2d";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDWa7YawpTVn6wYbFKh3hPpHxqSftuHfZSkw/6by2I+I";
   users = [ riadh nixos ];
   systems = [ ];
 in {
